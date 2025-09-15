e60aeb7c12a2a01ac6a6446118e1a08194513abc
Nikola Jambrešić <nikolajambresic8@gmail.com>
2025-09-15 22:36:32 +0200

    Part 2 of my JS journey
    Math library + roll the dice game

---
+++ jsPart2/script.js
@@ -0,0 +1,24 @@
+/*
+Math = lots of useful math constants and functions
+
+const PI = Math.PI;
+console.log(PI);*/
+
+/*
+Roll the dice game:
+
+        HTML:
+        <div class="container">
+            <h1>Roll the dice!</h1>
+            <button id="rollButton">Roll</button>
+            <h3 id="rollResult"></h3>
+        </div>
+
+
+let min = 1, max = 6, rollResult;
+
+document.getElementById("rollButton").onclick = function() {
+    rollResult = Math.floor(Math.random() * max) + min;
+    document.getElementById("rollResult").textContent = rollResult;
+}*/
+
