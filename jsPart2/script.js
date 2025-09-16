f5b3d9ed51da8e5b3b1c649cae87074ef5888ad6
Nikola Jambrešić <nikolajambresic8@gmail.com>
2025-09-17 00:47:02 +0200

    Update script.js
    If-else statements, ternary operator, switch-case

--- jsPart2/script.js
+++ jsPart2/script.js
@@ -22,3 +22,60 @@ document.getElementById("rollButton").onclick = function() {
     document.getElementById("rollResult").textContent = rollResult;
 }*/
 
+/*
+        HTML:
+        <div class="container">
+            <h3>Favourite german car brand?</h3>
+            <input type="radio" id="bmw" name="germanCars">
+            <label for="bmw">BMW</label><br>
+            <input type="radio" id="mercedes" name="germanCars">
+            <label for="mercedes">Mercedes</label><br>
+            <input type="radio" id="audi" name="germanCars">
+            <label for="audi">Audi</label><br><br>
+
+            <button id="submitButton">Submit</button><br><br>
+
+            <p id="message"></p>
+        </div>
+
+
+const bmw = document.getElementById("bmw");
+const mercedes = document.getElementById("mercedes");
+const audi = document.getElementById("audi");
+const submitButton = document.getElementById("submitButton");
+const message = document.getElementById("message");
+
+submitButton.onclick = function() {
+    if (bmw.checked) {
+        message.textContent = "You are a legend.";
+    } else if (mercedes.checked) {
+        message.textContent = "I see, you like luxury.";
+    } else {
+        message.textContent = "I see, you like road trips and snow.";
+    }
+}*/
+
+/*let age = 19;
+let message = age < 18 ? "You must be 18+ to enter the site!" : "Welcome broski!";
+console.log(message);*/
+
+/*let testResult = 46, grade;
+
+switch(true) {
+    case testResult >= 90:
+        grade = 5;
+        break;
+    case testResult >= 78:
+        grade = 4;
+        break;
+    case testResult >= 60:
+        grade = 3;
+        break;
+    case testResult >=45:
+        grade = 2;
+        break;
+    default:
+        grade = 1;
+}
+
+console.log(grade);*/
