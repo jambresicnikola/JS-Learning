e1df412536724a741dec113eab69f2371ec29360
Nikola Jambrešić <nikolajambresic8@gmail.com>
2025-09-26 00:22:31 +0200

    Part 5!
    callback, .forEach(), .map(), .filter(), .reduce(), function expressions, arrow functions

---
+++ jsPart5/script.js
@@ -0,0 +1,56 @@
+/*function multiply(callback, x, y) {
+    let result = x * y;
+    callback(result);
+}
+
+function displayOnPage(result) {
+    document.getElementById("h3").textContent = result;
+}
+
+multiply(displayOnPage, 5, 5);*/
+
+
+
+/*let cars = ["mercedes", "BMW", "PORSCHE", "AuDi", "oPEL"];
+
+cars.forEach(capitalize);
+cars.forEach(displayCars);
+
+function capitalize(element, index, array) {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+}
+
+function displayCars(element) {
+    console.log(element);
+}*/
+
+
+
+/*const dates = ["2020-1-10", "2021-2-20", "2023-3-30"];
+
+function formatDates(element) {
+    let parts = element.split("-");
+    return parts[2] + "." + parts[1] + "." + parts[0] + ".";
+}
+
+const formatedDates = dates.map(formatDates);
+
+console.log(formatedDates);*/
+
+
+
+/*const words = ["apple", "laptop", "university", "science", "picture"];
+
+const wordsAboveSix = words.filter((element) => element.length > 6);
+
+console.log(wordsAboveSix);*/
+
+
+
+/*const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let result = numbers.reduce(function(previous, next) {
+    return previous + next;
+});
+
+console.log(result);*/
